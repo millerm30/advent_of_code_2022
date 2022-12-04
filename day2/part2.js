ee08907800e69c2adef52ea6a8cef84f64ff4f80
millerm30 <millergm30@gmail.com>
2022-12-04 11:54:01 -0330

    .

--- day2/part2.js
+++ day2/part2.js
@@ -12,23 +12,23 @@ for (let play of playsArray) {
   // The plan is to lose
 
   if (myPlan == "X") {
-    console.log("Plan is to lose.");
+    console.log("Plan is to lose!");
 
     if (opponentPick == "A") {
       console.log(
-        "opponent picks rock and plan is to lose. So, you pick scissors."
+        "Opponent picks rock and plan is to lose. So, you pick scissors."
       );
       myPoints = myPoints + 3 + 0;
     }
     if (opponentPick == "B") {
       console.log(
-        "opponent picks paper and plan is to lose. So, you pick rock."
+        "Opponent picks paper and plan is to lose. So, you pick rock."
       );
       myPoints = myPoints + 1 + 0;
     }
     if (opponentPick == "C") {
       console.log(
-        "opponent picks scissors and plan is to lose. So, you pick paper."
+        "Opponent picks scissors and plan is to lose. So, you pick paper."
       );
       myPoints = myPoints + 2 + 0;
     }
@@ -37,23 +37,23 @@ for (let play of playsArray) {
   // The plan is to draw
 
   if (myPlan == "Y") {
-    console.log("Plan is to draw.");
+    console.log("Plan is to draw!");
 
     if (opponentPick == "A") {
       console.log(
-        "opponent picks rock and plan is to draw. So, you pick rock."
+        "Opponent picks rock and plan is to draw. So, you pick rock."
       );
       myPoints = myPoints + 1 + 3;
     }
     if (opponentPick == "B") {
       console.log(
-        "opponent picks paper and plan is to draw. So, you pick paper."
+        "Opponent picks paper and plan is to draw. So, you pick paper."
       );
       myPoints = myPoints + 2 + 3;
     }
     if (opponentPick == "C") {
       console.log(
-        "opponent picks scissors and plan is to draw. So, you pick scissors."
+        "Opponent picks scissors and plan is to draw. So, you pick scissors."
       );
       myPoints = myPoints + 3 + 3;
     }
@@ -62,23 +62,23 @@ for (let play of playsArray) {
   // The plan is to win
 
   if (myPlan == "Z") {
-    console.log("Plan is to draw.");
+    console.log("Plan is to win!");
 
     if (opponentPick == "A") {
       console.log(
-        "opponent picks rock and plan is to win. So, you pick paper."
+        "Opponent picks rock and plan is to win. So, you pick paper."
       );
       myPoints = myPoints + 2 + 6;
     }
     if (opponentPick == "B") {
       console.log(
-        "opponent picks paper and plan is to win. So, you pick scissors."
+        "Opponent picks paper and plan is to win. So, you pick scissors."
       );
       myPoints = myPoints + 3 + 6;
     }
     if (opponentPick == "C") {
       console.log(
-        "opponent picks scissors and plan is to win. So, you pick rock."
+        "Opponent picks scissors and plan is to win. So, you pick rock."
       );
       myPoints = myPoints + 1 + 6;
     }
