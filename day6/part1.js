1658188365c05a154ed4b117f419ed38eaf4326e
millerm30 <millergm30@gmail.com>
2022-12-06 08:02:44 -0330

    day 6

---
+++ day6/part1.js
@@ -0,0 +1,33 @@
+// Day 6:
+
+const fs = require('fs');
+const input = fs.readFileSync('./input.txt', 'utf8')
+
+const getLastCharacters = (amount, characters) => {
+  characters = characters.slice(characters.length - amount, characters.length);
+  let lastCharacters = [];
+  for (let character of characters) {
+    lastCharacters.push(character);
+  }
+  return lastCharacters;
+};
+
+const findStartPoints = ( markerLength, input ) => {
+  let letters = [];
+  let counter = 0;
+  for (const letter of input) {
+    counter++;
+    letters.push(letter);
+    let lastCharacters = getLastCharacters(markerLength, letters);
+    let isUnique = [...new Set(lastCharacters)];
+    if (isUnique.length === markerLength && !isUnique.includes(undefined)) {
+      console.log(counter);
+      return;
+    }
+  }
+}
+
+findStartPoints(4, input);
+findStartPoints(14, input);
+
+
