dd08a6141220344dc81f5dbe099dbf0d49fea89a
millerm30 <millergm30@gmail.com>
2022-12-10 13:53:05 -0330

    day 10 setup...

---
+++ day10/solution.js
@@ -0,0 +1,19 @@
+// Day 10: Cathode-Ray Tibe //
+const fs = require("fs");
+
+const inputs = fs
+  .readFileSync("smallTest", "utf-8")
+  .replace(/\r/g, "")
+  .trim()
+  .split("\n");
+
+const partOne = () => {
+  console.log("This is part one!")
+};
+
+const partTwo = () => {
+  console.log("This is part two!")
+};
+
+partOne();
+partTwo();
