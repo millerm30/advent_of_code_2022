7dbf80957d3a7c61768e1cfa39dde892556f1726
millerm30 <millergm30@gmail.com>
2022-12-11 19:50:27 -0330

    solution day 11 complete

--- day11/solution.js
+++ day11/solution.js
@@ -10,8 +10,6 @@ const getOperationFunction = (input) => {
 
 const getMonkeys = () => {
   const monkeys = readFileSync("input.txt", { encoding: "utf-8" })
-    .replace(/\r/g, "") 
-    .trim() 
     .split("\n\n")
     .map((lines, monkeyId) => {
       const items = lines
@@ -45,24 +43,30 @@ const getMonkeys = () => {
 const partOne = () => {
   const monkeys = getMonkeys();
   for (let i = 0; i < 20; i++) {
-    for (const monkey of monkeys) {
+    monkeys.forEach((monkey) => {
       let items = monkey.items;
       while (items.length) {
         let item = items.shift();
         monkey.totalInspectedObjects++;
-
+        
         item = monkey.operation(item);
         item = Math.floor(item / 3);
         const destination = monkey.sendTo(item);
 
         monkeys[destination].items.push(item);
       }
-    }
+    });
   }
+  const ids = monkeys.map((m) => m.id);
   const activity = monkeys.map((m) => m.totalInspectedObjects);
-  activity.sort((a, b) => b - a);
-  console.log(activity[0] * activity[1]);
+  const combined = ids.map((id, index) => ({ id, activity: activity[index] }));
+  combined.sort((a, b) => b.activity - a.activity);
+  const topTwo = combined.slice(0, 2);
+  console.log(
+    `Monkey ${topTwo[0].id} inspected ${topTwo[0].activity} objects and Monkey ${topTwo[1].id} inspected ${topTwo[1].activity} objects. The multiplied answer is ${topTwo[0].activity * topTwo[1].activity}`
+  );
 };
+
 
 const partTwo = () => {
   const monkeys = getMonkeys();
@@ -82,9 +86,15 @@ const partTwo = () => {
       }
     }
   }
+  const ids = monkeys.map((m) => m.id);
   const activity = monkeys.map((m) => m.totalInspectedObjects);
+  const combined = ids.map((id, index) => ({ id, activity: activity[index] }));
+  combined.sort((a, b) => b.activity - a.activity);
+  const topTwo = combined.slice(0, 2);
   activity.sort((a, b) => b - a);
-  console.log(activity[0] * activity[1]);
+  console.log(
+    `Monkey ${topTwo[0].id} inspected ${topTwo[0].activity} objects and Monkey ${topTwo[1].id} inspected ${topTwo[1].activity} objects. The multiplied answer is ${topTwo[0].activity * topTwo[1].activity}`
+  );
 };
 
 partOne();
