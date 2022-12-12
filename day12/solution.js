f0f761b8e6b9f9051796b5048e9758bf80f2e48a
millerm30 <millergm30@gmail.com>
2022-12-11 21:46:53 -0330

    ...

--- day12/solution.js
+++ day12/solution.js
@@ -3,13 +3,8 @@ import { readFileSync } from "fs";
 const input = readFileSync("testData.txt", "utf-8")
   .replace(/\r/g, "")
   .trim()
-  // split the individual words in a line
-  .split(" ")
-  .map((word) => {
-    // split the words into an array
-    const words = word.split(/([0-9]+)/);
-    return words;
-  });
+  .split("\n")
+  
 
 
 console.log(input);
